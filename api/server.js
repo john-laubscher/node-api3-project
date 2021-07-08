523b87ab420ddab531c7d7d27a7d1bc1fe2a0e87
John Laubscher <jlmemphiscommanders@gmail.com>
2021-07-08 17:06:24 -0500

    changes for heroku

--- api/server.js
+++ api/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
@@ -6,8 +6,12 @@ const server = express();
 
 // global middlewares and the user's router need to be connected here
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+server.get("/api", (req, res) => {
+  res.send(`<h2> this is the server.api</h2>`);
 });
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // require your server and launch it
+/////////code for adding to Heroku/////////
+require("dotenv").config();
+// const express = require("express");
+
+if (process.env.NODE_ENV === "development") {
+  const cors = require("cors");
+  server.use(cors());
+}
+
+// server.use("*", (req, res) => {
+//   res.send("<h1>success<h1>");
+// });
+
+// server.use(express.static(path.join(__dirname,'client/build')))  ////this is one of the instructions from Shweta
+
+const PORT = process.env.PORT || 7000;
+
+const server = require("./api/server");
+
+server.listen(PORT, () => {
+  console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
+});
