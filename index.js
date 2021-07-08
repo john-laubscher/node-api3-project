6e051d8d238b4744c34c352d1bf54fb0621cd05e
John Laubscher <jlmemphiscommanders@gmail.com>
2021-07-08 16:12:49 -0500

    changes for heroku

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 // require your server and launch it
+/////////code for adding to Heroku/////////
+require("dotenv").config();
+const express = require("express");
+
+if (process.env.NODE_ENV === "development") {
+  const cors = require("cors");
+  server.use(cors());
+}
+
+server.use("*", (req, res) => {
+  res.send("<h1>success<h1>");
+});
+
+const PORT = process.env.PORT || 7000;
+
 const server = require("./api/server");
 
-server.listen(7000, () => {
-  console.log("\n*** Server Running on http://localhost:7000 ***\n");
+server.listen(PORT, () => {
+  console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
 });
